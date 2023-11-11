6225449c0140c53875033122c3f05b3297ab635e
kreatiff <oleg.estrin@gmail.com>
2023-11-12 00:27:58 +1000

    remove dist

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: "dist",
-};
-
+const nextConfig = {};
 module.exports = nextConfig
+
+// Compare this snippet from app/questions/page.js:
