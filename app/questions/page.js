1db78ed9af7a6d50479765406f3e45b1a74b6420
kreatiff <oleg.estrin@gmail.com>
2023-11-11 23:51:13 +1000

    next js update

--- app/questions/page.js
+++ app/questions/page.js
@@ -11,8 +11,10 @@ import { ConfirmDialog } from "primereact/confirmdialog";
 import { Toast } from "primereact/toast";
 import OpenAI from "openai";
 //test
+const fetchedApiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: fetchedApiKey,
 });
 
 const generateRandomId = () => {
